90696d53d00572ecdfb5473e2c693f4adab1121c
Anil Zeybek <anil.zeybek98@gmail.com>
2023-08-23 13:44:23 +0200

    return to xor torch

--- pytorch.py
+++ pytorch.py
@@ -1,67 +1,48 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Model(nn.Module):
+class NN(nn.Module):
     def __init__(self, input_size, output_size):
         super().__init__()
 
-        self.arch = nn.Sequential(
-            nn.Linear(input_size, 16),
-            nn.ReLU(),
-            nn.Linear(16, 16),
-            nn.ReLU(),
-            nn.Linear(16, output_size),
-        )
+        self.arch = nn.Sequential(nn.Linear(input_size, 2), nn.Sigmoid(), nn.Linear(2, output_size), nn.Sigmoid())
 
     def forward(self, x):
         return self.arch(x)
 
 
-def generate_adder_data(n_samples=32, max_value=50):
-    a = np.random.randint(0, max_value, n_samples)
-    b = np.random.randint(0, max_value, n_samples)
-    sum = a + b
-
-    return np.column_stack((a, b)), sum
-
-
 def main():
-    model = Model(input_size=2, output_size=1)
-    optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-4)
+    torch.random.manual_seed(0)
+    data = torch.tensor(
+        [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 0],
+        ],
+        dtype=torch.float32,
+    )
 
-    for i in range(10000):
-        inp, label = generate_adder_data(n_samples=32)
+    model = NN(input_size=2, output_size=1)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1)
+    in_data = data[:, :-1]
+    label = data[:, -1]
 
-        inp = torch.tensor(inp, dtype=torch.float32)
-        label = torch.tensor(label, dtype=torch.float32)
+    for i in range(2000):
+        pred = model(in_data).squeeze()
 
-        pred = model(inp).squeeze()
         loss = F.mse_loss(pred, label)
-
-        optimizer.zero_grad()
         loss.backward()
+
         optimizer.step()
 
         if (i + 1) % 100 == 0:
-            print(f"epoch: {i+1} loss: {loss.item()}")
+            print(f"epoch: {i+1} loss: {loss}")
 
-    print("==========")
-
-    inp, label = generate_adder_data(n_samples=5)
-    inp = torch.tensor(inp, dtype=torch.float32)
-    label = torch.tensor(label, dtype=torch.float32)
-
-    out = model(inp)
-    print("PREDICTIONS:")
-    for i in range(inp.shape[0]):
-        a = int(inp[i][0].item())
-        b = int(inp[i][1].item())
-        pred = round(out[i].item())
-
-        print(f"{a} + {b} = {pred}, real={a+b}")
+    for i in data:
+        print(f"{i[0]} ^ {i[1]} = {model(i[:2]).item():.3f}")
 
 
 if __name__ == "__main__":
